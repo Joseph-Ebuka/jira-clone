8a641b7a28f9bb53cc94a42acaa791edf6a5028f
sureboyhtml <ebukaj665@gmail.com>
2024-11-14 14:52:51 +0100

    add: custom button libary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div className="">
-      <Button>Click Me</Button>
-      <p className="text-blue-600 text-md  "> Ebuka test</p>
+    <div className="flex gap-4 ">
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="teritary">Teritary</Button>
+      <Button variant="outline">Outline</Button>
     </div>
   );
 }
