58ca1781b8f80a752a3411ad846daecb0b67a98a
sureboyhtml <ebukaj665@gmail.com>
2024-11-15 08:21:27 +0100

    add: inputs

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -3,20 +3,45 @@ import { FaGithub } from "react-icons/fa";
 
 import { DottedSeperator } from "@/components/dotted-seperator";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
-export const SignInCard = () => {
+export const SignUpCard = () => {
   return (
     <Card className="w-full h-full md:w-[486px] border-none shadow-none">
       <CardHeader className="flex items-center justify-center p-7">
-        <CardTitle className="text-2xl">Welcome back!</CardTitle>
+        <CardTitle className="text-2xl">Sign Up</CardTitle>
+        <CardDescription>
+          By signing up you agree to our{" "}
+          <Link href="/privacy">
+            <span className="text-blue-700">Privacy Policy</span>
+          </Link>{" "}
+          and {" "}
+          <Link href="/terms">
+            <span className="text-blue-700">Terms of service</span>
+          </Link>{" "}
+        </CardDescription>
       </CardHeader>
       <div className="px-7">
         <DottedSeperator />
       </div>
       <CardContent className="p-7">
         <form className="space-y-4">
+          <Input
+            required
+            type="text"
+            value=""
+            onChange={() => {}}
+            placeholder="Enter your name"
+            disabled={false}
+          />
           <Input
             required
             type="email"
