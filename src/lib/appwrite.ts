10845a87d787b1ca101c9a1a8034f806c0afa6da
sureboyhtml <ebukaj665@gmail.com>
2024-11-16 12:22:23 +0100

    new: appwrite client

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,13 @@
+import { Client, Account, Databases, Storage, Users } from "node-appwrite";
+
+export const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+    .setKey(process.env.NEXT_APPWRITE_KEY!);
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+};
