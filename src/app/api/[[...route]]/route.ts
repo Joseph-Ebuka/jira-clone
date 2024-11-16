8d84d6f13193280db0cf99f92ffe325b5745a8c8
sureboyhtml <ebukaj665@gmail.com>
2024-11-16 09:13:07 +0100

    fix: login route

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,13 +1,12 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
-import auth from "@/features/auth/server/route"
+import auth from "@/features/auth/server/route";
 
 const app = new Hono().basePath("/api");
 
-const routes = app
-.route("/auth", auth)
+const routes = app.route("/auth", auth);
 
 export const GET = handle(app);
 
-export type Apptype = typeof routes
+export type Apptype = typeof routes;
