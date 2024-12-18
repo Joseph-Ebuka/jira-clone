f7e33b7181b33ba066413863e07881aeade7892c
sureboyhtml <ebukaj665@gmail.com>
2024-12-18 14:52:53 +0100

    fix: css styles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,13 +4,13 @@ export const Navbar = () => {
   return (
     <nav className="pt-4 px-6 flex items-center justify-between">
       <div className=" flex-col hidden lg:flex">
-        <h1 className="text-2xl font-semibold">
-          <p className="text-muted-foreground">
-            Monitor all of your projects and taks
-          </p>
-        </h1>
+        <h1 className="text-2xl font-semibold">Home</h1>
+        <p className="text-muted-foreground">
+          {" "}
+          Monitor all of your projects and taks{" "}
+        </p>
       </div>
-      <UserButton/>
+      <UserButton />
     </nav>
   );
 };
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,7 +5,7 @@ import { Navigation } from "./navigation";
 
 export const Sidebar = () => {
   return (
-    <aside className="h-full bg-neutral-100 p-4 w-full">
+    <aside className="h-full bg-neutral-100 p-4 ">
       <Link href="/">
         <Image src="logo.svg" alt="logo" width={164} height={48} />
       </Link>
