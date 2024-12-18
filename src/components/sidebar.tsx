c6ffaa4436ea43b5cb64c4f758126f0acc395e59
sureboyhtml <ebukaj665@gmail.com>
2024-12-18 14:05:52 +0100

    new: sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,16 @@
-export const Sidebar = ()=>{
-    return(
-        <aside>
-            SideBar
-        </aside>
-    )
-}
+import Image from "next/image";
+import Link from "next/link";
+import { DottedSeperator } from "./dotted-seperator";
+import { Navigation } from "./navigation";
+
+export const Sidebar = () => {
+  return (
+    <aside className="h-full bg-neutral-100 p-4 w-full">
+      <Link href="/">
+        <Image src="logo.svg" alt="logo" width={164} height={48} />
+      </Link>
+      <DottedSeperator className="my-4" />
+      <Navigation />
+    </aside>
+  );
+};
